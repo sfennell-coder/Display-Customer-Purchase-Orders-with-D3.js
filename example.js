3c0a0c297ce5db453e4cdbedee57827bfea617c6
Scott Fennell <slfennell01@gmail.com>
2024-06-17 10:08:03 -0400

    Implement data loading and DOM manipulation with D3.js.
    Additions to HTM fileL: ul tag. 
    Additions to js file: access csv, data, li stored in ul, and text (d3).

--- example.js
+++ example.js
@@ -1,4 +1,15 @@
 // U86364709
 function main(){
     // d3 code goes here
+
+    d3.dsv(',','data/purchase_orders').then(function(data){ // access csv file
+        var ul = d3.select('#purchaseOrders_storage'); // variable for ul. In order to access to append li's
+
+        ul.selectAll('li')
+            .data(data) // get data from csv file
+            .enter()
+            .append('li') // add li to ul
+            .text(d=> {return `Customer ${d.customerName} - Order #${d.orderID}: $${d.purchaseAmount}`})
+
+    })
 }
