c361bb5f7f03a5290c0692185a9ec558df1c7a0b
Scott Fennell <slfennell01@gmail.com>
2024-06-17 12:18:55 -0400

    Added try/catch incase of error 404 csv file not found

--- example.js
+++ example.js
@@ -1,15 +1,14 @@
 // U86364709
 function main(){
     // d3 code goes here
+    let error;
+    d3.dsv(',','data/purchase_orders.csv').then(function(data){ // access csv file    
+        var ul = d3.select('#purchaseOrders_storage'); // variable for ul. In order to access to append li's 
 
-    d3.dsv(',','data/purchase_orders').then(function(data){ // access csv file
-        var ul = d3.select('#purchaseOrders_storage'); // variable for ul. In order to access to append li's
-
-        ul.selectAll('li')
+        ul.selectAll('ul')
             .data(data) // get data from csv file
             .enter()
             .append('li') // add li to ul
-            .text(d=> {return `Customer ${d.customerName} - Order #${d.orderID}: $${d.purchaseAmount}`})
-
-    })
+            .text(d=> {return (`Customer ${d.customerName} - Order #${d.orderId}: $${d.purchaseAmount}`)});
+    }).catch(()=>alert('Sorry! There must be a error retrieving the data!'), error);
 }
